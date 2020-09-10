e35f494dcc640374278988966e1a42334bd72b43
kldavis52 <kyle.linnard.davis@gmail.com>
2020-09-09 22:35:21 -0400

    heroku test 3

--- project/settings.py
+++ project/settings.py
@@ -152,6 +152,6 @@ INTERNAL_IPS = [
 ]
 
 # Configure Django App for Heroku.
-import django_heroku
-django_heroku.settings(locals())
-del DATABASES['default']['OPTIONS']['sslmode']
+# import django_heroku
+# django_heroku.settings(locals())
+# del DATABASES['default']['OPTIONS']['sslmode']
