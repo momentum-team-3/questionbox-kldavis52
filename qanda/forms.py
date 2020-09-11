ee652d22282f989630c3e9759b20f3572e60f6f6
kldavis52 <kyle.linnard.davis@gmail.com>
2020-09-11 11:43:26 -0400

    updated userprofile.html to show answers for each question

--- qanda/forms.py
+++ qanda/forms.py
@@ -1,1 +1,8 @@
-from django.forms import Form, ChoiceField, CharField
+from django import forms
+from .models import Answer, Tag
+
+class QuestionForm(forms.ModelForm):
+    pass
+ 
+class AnswerForm(forms.ModelForm):
+    pass
